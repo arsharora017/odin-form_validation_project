3febc3006a758437fbd67f7927df7b3e78041ca5
Arshdeep Arora <arsharora017@gmail.com>
2023-08-12 23:24:17 -0400

    validate ZIP code for selected country

--- form.js
+++ form.js
@@ -16,7 +16,7 @@ email.addEventListener("input", (event) => {
 
 function showError() {
   if (email.validity.valueMissing) {
-    //display message: Enter an email address
+    //display message
     emailError.textContent = "Enter a valid email address";
     emailError.style.display = "block";
   } else if (email.validity.typeMismatch) {
@@ -34,3 +34,49 @@ form.addEventListener("submit", (event) => {
     event.preventDefault();
   }
 });
+
+// ZIP validation
+function checkZIP() {
+  // For each country, defines the pattern that the ZIP has to follow
+  const constraints = {
+    ch: [
+      "^(CH-)?\\d{4}$",
+      "Switzerland ZIPs must have exactly 4 digits: e.g. CH-1950 or 1950",
+    ],
+    fr: [
+      "^(F-)?\\d{5}$",
+      "France ZIPs must have exactly 5 digits: e.g. F-75012 or 75012",
+    ],
+    de: [
+      "^(D-)?\\d{5}$",
+      "Germany ZIPs must have exactly 5 digits: e.g. D-12345 or 12345",
+    ],
+    nl: [
+      "^(NL-)?\\d{4}\\s*([A-RT-Z][A-Z]|S[BCE-RT-Z])$",
+      "Netherland ZIPs must have exactly 4 digits, followed by 2 letters except SA, SD and SS",
+    ],
+  };
+
+  const country = document.getElementById("Country").value;
+  const ZIPField = document.getElementById("ZIP");
+  const zipError = document.querySelector("#ZIPSpan");
+
+  // Build the constraint checker
+  const constraint = new RegExp(constraints[country][0], "");
+
+  if (!constraint.test(ZIPField.value)) {
+    //display error from constraints object
+    zipError.textContent = `${constraints[country][1]}`;
+    zipError.style.display = "block";
+  } else {
+    //remove error text
+    zipError.textContent = "";
+  }
+}
+
+// we link it to the onchange event for the <select> and the oninput event for the <input>
+//this is important to load values
+window.onload = () => {
+  document.getElementById("Country").onchange = checkZIP;
+  document.getElementById("ZIP").oninput = checkZIP;
+};
